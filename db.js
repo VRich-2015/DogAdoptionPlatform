d303d9a8b90e973d667ce11c08e6c67a8ed8bff7
VRich-2015 <vanesharich2015@gmail.com>
2025-05-23 20:17:39 -0400

    Update db.js

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ Connected to MongoDB"))
+  .then(() => console.log(" Connected to MongoDB"))
   .catch((err) => {
-    console.error("❌ MongoDB connection error:", err);
+    console.error(" MongoDB connection error:", err);
     process.exit(1);
   });
